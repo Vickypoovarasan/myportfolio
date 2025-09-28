788fe3a06b44000c8a21bb8fa2d97a73b50405d4
Vickypoovarasan <148315854+Vickypoovarasan@users.noreply.github.com>
2025-09-28 18:24:56 +0530

    portfolio version-1

---
+++ script.js
@@ -0,0 +1,28 @@
+
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+
+//   typewritter hero section
+
+document.addEventListener("DOMContentLoaded", () => {
+  const texts = ["Full Stack Developer", "UI/UX Designer", "Tech Enthusiast", "Creative Problem Solver"];
+  const span = document.getElementById("fade-text");
+  let i = 0;
+
+  setInterval(() => {
+    span.style.opacity = 0;
+    setTimeout(() => {
+      span.textContent = texts[i];
+      span.style.opacity = 1;
+      i = (i + 1) % texts.length;
+    }, 500);
+  }, 3000);
+});
+
+
+
+
