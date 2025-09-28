4feead71064b2315438a64a0f1eb296fe793407e
Vickypoovarasan <148315854+Vickypoovarasan@users.noreply.github.com>
2025-09-28 18:57:20 +0530

    navbar changed

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 
-  const hamburger = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-  hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("show");
+});
+
+// Close nav after clicking a link
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("show");
   });
+});
+
 
 //   typewritter hero section
 
